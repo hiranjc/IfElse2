6deb0ba304495631e36198cf36797033831bb02b
Hiran Ferreti Baccos <hiranjc1@outlook.com>
2025-09-18 17:44:23 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace IfElse2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Qual a hora atual? ");
+            int hora = int.Parse(Console.ReadLine());
+
+            if (hora < 12)
+            {
+                Console.WriteLine("Bom dia!");
+            }
+            else if (hora < 18)
+            {
+                Console.WriteLine("Boa tarde!");
+            }
+            else
+            {
+                Console.WriteLine("Boa noite!");
+            }
+        }
+    }
+}
